3a9daed78d36e5978e3cdf8e34640cec7b50cf26
Tamim Ahmed Raju <138282465+mrahmed17@users.noreply.github.com>
2024-08-19 18:57:28 +0600

    Update

--- Angular Project/Final Angular Project/For Customized Template/hr-payroll-management-system/src/app/authentication/login/login.component.ts
+++ Angular Project/Final Angular Project/For Customized Template/hr-payroll-management-system/src/app/authentication/login/login.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
